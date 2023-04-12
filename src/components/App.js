511f2260465fef47e5543b9e3b3937368946a604
Arnab-31 <57807670+Arnab-31@users.noreply.github.com>
2023-04-12 13:09:36 +0530

    Solve

--- src/components/App.js
+++ src/components/App.js
@@ -1,58 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
+import "../styles/App.css"
 
-const App = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const [loggedIn, setLoggedIn] = useState(false);
+const WORD_LIST = ['apple', 'banana', 'cherry', 'grape', 'orange'];
 
-  const handleSubmit = async event => {
-    event.preventDefault();
-    try {
-      const response = await fetch("https://reqres.in/api/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ email, password })
-      });
-      const data = await response.json();
-      if (data.error) {
-        setError(data.error);
-      } else {
-        setLoggedIn(true);
-      }
-    } catch (err) {
-      setError("An error occurred. Please try again later.");
-    }
-  };
+function App() {
+  const [word, setWord] = useState('');
+  const [flashWord, setFlashWord] = useState(true);
+  const [userInput, setUserInput] = useState('');
+  const [result, setResult] = useState('');
+  const [index, setIndex] = useState(0);
+
 
   return (
-    <div>
-      {loggedIn ? (
-        <h1>Welcome User!</h1>
-      ) : (
-        <form onSubmit={handleSubmit}>
-          <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-            required
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            required
-          />
-          <button type="submit">Submit</button>
-          {error && <p style={{ color: "red" }}>{error}</p>}
-        </form>
+    <div class="mini-game-container">
+      <h2 class="mini-game-title">Mini Game</h2>
+      <p class="mini-game-word">{word}</p>
+      <form class="mini-game-form" onSubmit={handleFormSubmit}>
+        <input class="mini-game-input" type="text" value={userInput} onChange={handleInputChange} />
+        <button class="mini-game-button" type="submit">Check Answer</button>
+      </form>
+      {result && (
+        <>
+          <p class="mini-game-result">{result}</p>
+          <button class="mini-game-restart-button" onClick={handleRestartClick}>Restart</button>
+        </>
       )}
     </div>
   );
-};
+}
 
 export default App;
